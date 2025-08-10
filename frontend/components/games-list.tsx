b9e562d0aa1d38f3c39e9c836f8959d2fa962659
diyrad167 <diyangraditya144@gmail.com>
2025-08-10 09:34:12 +0700

    feat: add additional details

--- frontend/components/games-list.tsx
+++ frontend/components/games-list.tsx
@@ -57,7 +57,7 @@ export function GamesList({ games }: { games: Game[] }) {
                 href="/create"
                 className="rounded-lg bg-blue-500 px-4 py-2 text-sm font-medium text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
               >
-                Create New Game
+                Join Available Games
               </Link>
             </div>
           ) : (
